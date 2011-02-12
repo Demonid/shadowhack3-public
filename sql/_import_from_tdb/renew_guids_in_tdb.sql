a7482b62897ed24a24c4807daa84ec916bbebe46
Raziel <Raziel@localhost>
2011-02-12 23:45:51 +0200

    linked_respawn, creature_formations ? difficulty_entry_? ???? ????? ????????? ??????????

--- sql/_import_from_tdb/renew_guids_in_tdb.sql
+++ sql/_import_from_tdb/renew_guids_in_tdb.sql
@@ -2,14 +2,27 @@ SET @MAPID := 631;
 
 ALTER TABLE `waypoint_data` ADD INDEX `idx_id` (`id`) ;
 ALTER TABLE `creature_addon` ADD INDEX `idx_path` (`path_id`) ;
+ALTER TABLE `creature_template`
+ADD INDEX `idx_diff1` (`difficulty_entry_1`),
+ADD INDEX `idx_diff2` (`difficulty_entry_2`),
+ADD INDEX `idx_diff3` (`difficulty_entry_3`);
+
 DELETE FROM `creature` WHERE `map` <> @MAPID;
 DELETE FROM `creature_addon` WHERE `guid` NOT IN (SELECT `guid` FROM `creature`);
+DELETE FROM `linked_respawn` WHERE `linkType` = 0 AND `guid` NOT IN (SELECT `guid` FROM `creature`);
+DELETE FROM `linked_respawn` WHERE `linkType` = 0 AND `linkedGuid` NOT IN (SELECT `guid` FROM `creature`);
+DELETE FROM `creature_formations` WHERE `leaderGUID` NOT IN (SELECT `guid` FROM `creature`);
+DELETE FROM `creature_formations` WHERE `memberGUID` NOT IN (SELECT `guid` FROM `creature`);
 DELETE FROM `waypoint_data` WHERE `id` NOT IN (SELECT `path_id` FROM `creature_addon`);
 
 DROP TRIGGER IF EXISTS `creature_after_update`;
 CREATE TRIGGER `creature_after_update` AFTER UPDATE ON `creature`
 FOR EACH ROW BEGIN
 UPDATE `creature_addon` SET `guid`=NEW.guid WHERE `guid`=OLD.guid;
+UPDATE `creature_formations` SET `leaderGUID`=NEW.guid WHERE `leaderGUID`=OLD.guid;
+UPDATE `creature_formations` SET `memberGUID`=NEW.guid WHERE `memberGUID`=OLD.guid;
+UPDATE `linked_respawn` SET `guid`=NEW.guid WHERE `linkType` = 0 AND `guid`=OLD.guid;
+UPDATE `linked_respawn` SET `linkedGuid`=NEW.guid WHERE `linkType` = 0 AND `linkedGuid`=OLD.guid;
 END;
 SELECT @MAPID * 1000 INTO @temp_var;
 UPDATE `creature` SET `guid` = @MAPID * 1000 WHERE `guid`=0;
