f4b721cfcab01a6e5ea3692a2a21852c6a7fb369
crawling <crawling@localhost>
2011-03-07 19:04:41 +0700

    (61) Antipereliv, antidodge, marki bg, hk ranks, scriploader, stoneclaw totem, duel reset cd, brackets enabling,

--- src/server/game/Battlegrounds/Battleground.h
+++ src/server/game/Battlegrounds/Battleground.h
@@ -483,6 +483,9 @@ class Battleground
         void RemoveAuraOnTeam(uint32 SpellID, uint32 TeamID);
         void RewardHonorToTeam(uint32 Honor, uint32 TeamID);
         void RewardReputationToTeam(uint32 faction_id, uint32 Reputation, uint32 TeamID);
+        void RewardMark(Player *plr,uint32 count);
+        void SendRewardMarkByMail(Player *plr,uint32 mark, uint32 count);
+        void RewardItem(Player *plr, uint32 item_id, uint32 count);
         void UpdateWorldState(uint32 Field, uint32 Value);
         void UpdateWorldStateForPlayer(uint32 Field, uint32 Value, Player *Source);
         void EndBattleground(uint32 winner);
