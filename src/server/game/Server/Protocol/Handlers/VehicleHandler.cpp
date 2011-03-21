79c4a669ecd2490598b9df78d8f918e0c8cc2e06
Shauren <shauren.trinity@gmail.com>
2011-03-21 19:37:21 +0100

    Core/Vehicles: Fixed assertion failures related to seat changes, huge thanks to Machiavelli for well documented vehicle code and j4r0d for providing info how to reproduce the crash

--- src/server/game/Server/Protocol/Handlers/VehicleHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/VehicleHandler.cpp
@@ -99,7 +99,7 @@ void WorldSession::HandleChangeSeatsOnControlledVehicle(WorldPacket &recv_data)
             {
                 if (Vehicle *vehicle = vehUnit->GetVehicleKit())
                     if (vehicle->HasEmptySeat(seatId))
-                        GetPlayer()->_EnterVehicle(vehicle, seatId);
+                        vehUnit->HandleSpellClick(GetPlayer(), seatId);
             }
             break;
         }
@@ -116,7 +116,7 @@ void WorldSession::HandleChangeSeatsOnControlledVehicle(WorldPacket &recv_data)
             else if (Unit *vehUnit = Unit::GetUnit(*GetPlayer(), guid))
                 if (Vehicle *vehicle = vehUnit->GetVehicleKit())
                     if (vehicle->HasEmptySeat(seatId))
-                        GetPlayer()->_EnterVehicle(vehicle, seatId);
+                        vehUnit->HandleSpellClick(GetPlayer(), seatId);
             break;
         }
         default:
