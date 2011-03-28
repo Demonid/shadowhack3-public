7e4e39a7581d66bfc68b4b9625c6156e70e60704
ungaminga <ungaminga@localhost>
2011-03-28 13:20:49 +0300

    (133) ?????? ? ?????? ??????.

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -2743,6 +2743,7 @@ void WorldObject::GetNearPoint(WorldObject const* searcher, float &x, float &y, 
         searcher->UpdateAllowedPositionZ(x, y, z);            // update to LOS height if available
     else
         UpdateGroundPositionZ(x, y, z);
+    z +=0.2;
 }
 
 void WorldObject::MovePosition(Position &pos, float dist, float angle)
