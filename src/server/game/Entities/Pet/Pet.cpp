c6fdb0f51ceab69240b6c57ebb24cd9e451c7cda
ungaminga <ungaminga@ungaminga-PC>
2011-05-06 07:14:00 +0300

    (236) ?????? ? ????? ?????.

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -296,7 +296,7 @@ bool Pet::LoadPetFromDB(Player* owner, uint32 petentry, uint32 petnumber, bool c
     InitTalentForLevel();                                   // set original talents points before spell loading
 
     uint32 timediff = uint32(time(NULL) - fields[14].GetUInt32());
-    _LoadAuras(timediff);
+    CastPetAuras(current);
 
     // load action bar, if data broken will fill later by default spells.
     if (!is_temporary_summoned)
@@ -308,7 +308,6 @@ bool Pet::LoadPetFromDB(Player* owner, uint32 petentry, uint32 petnumber, bool c
         _LoadSpellCooldowns();
         LearnPetPassives();
         InitLevelupSpellsForLevel();
-        CastPetAuras(current);
     }
 
     CleanupActionBar();                                     // remove unknown spells from action bar after load
