ecebcf11a491f6f36ede04b26cfdfa323d503866
Admin <Admin@Twink.5hat.net>
2012-01-21 13:10:58 +0200

    after duel reset hp

---
+++ src/server/scripts/Northrend/Ulduar/ulduar/ulduar.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DEF_ULDUAR_H
+#define DEF_ULDUAR_H
+
+enum Encounter
+{
+    BOSS_LEVIATHAN,
+    BOSS_IGNIS,
+    BOSS_RAZORSCALE,
+    BOSS_XT002,
+    BOSS_ASSEMBLY,
+    BOSS_KOLOGARN,
+    BOSS_AURIAYA,
+    BOSS_MIMIRON,
+    BOSS_HODIR,
+    BOSS_THORIM,
+    BOSS_FREYA,
+    BOSS_VEZAX,
+    BOSS_YOGGSARON,
+    BOSS_ALGALON,
+    BOSS_MINIBOSS,
+    MAX_ENCOUNTER
+};
+
+enum Data64
+{
+    DATA_LEVIATHAN,
+    DATA_NORGANNON,
+    DATA_IGNIS,
+    DATA_RAZORSCALE,
+    DATA_EXP_COMMANDER,
+    DATA_XT002,
+    DATA_STEELBREAKER,
+    DATA_MOLGEIM,
+    DATA_BRUNDIR,
+    DATA_KOLOGARN,
+    DATA_AURIAYA,
+    DATA_BRIGHTLEAF,
+    DATA_IRONBRANCH,
+    DATA_STONEBARK,
+    DATA_FREYA,
+    DATA_THORIM,
+    DATA_RUNIC_COLOSSUS,
+    DATA_RUNE_GIANT,
+    DATA_MIMIRON,
+    DATA_LEVIATHAN_MK_II,
+    DATA_VX_001,
+    DATA_AERIAL_UNIT,
+    DATA_MAGNETIC_CORE,
+    DATA_VEZAX,
+    DATA_YS_FREYA,
+    DATA_YS_THORIM,
+    DATA_YS_MIMIRON,
+    DATA_YS_HODIR,
+    DATA_YOGGSARON_BRAIN,
+    DATA_YOGGSARON
+};
+
+enum Data
+{
+    DATA_LEVIATHAN_DOOR,
+    DATA_TOWER_DESTROYED,
+    DATA_RUNIC_DOOR,
+    DATA_STONE_DOOR,
+    DATA_CALL_TRAM,
+    DATA_MIMIRON_ELEVATOR,
+    DATA_HODIR_RARE_CHEST
+};
+
+enum eBossNPCs
+{
+    NPC_LEVIATHAN               = 33113,
+    NPC_IGNIS                   = 33118,
+    NPC_RAZORSCALE              = 33186,
+    NPC_EXPEDITION_COMMANDER    = 33210,
+    NPC_XT002                   = 33293,
+    NPC_STEELBREAKER            = 32867,
+    NPC_MOLGEIM                 = 32927,
+    NPC_BRUNDIR                 = 32857,
+    NPC_KOLOGARN                = 32930,
+    NPC_KOLOGARN_BRIDGE         = 34297,
+    NPC_RUBBLE                  = 33768,
+    NPC_AURIAYA                 = 33515,
+    NPC_MIMIRON                 = 33350,
+    NPC_HODIR                   = 32845,
+    NPC_THORIM                  = 32865,
+    NPC_FREYA                   = 32906,
+    NPC_VEZAX                   = 33271,
+    NPC_YOGGSARON               = 33288,
+    NPC_ALGALON                 = 32871,
+};
+
+enum eTowerEvents
+{
+    EVENT_TOWER_OF_STORM_DESTROYED     = 21031,
+    EVENT_TOWER_OF_FROST_DESTROYED     = 21032,
+    EVENT_TOWER_OF_FLAMES_DESTROYED    = 21033,
+    EVENT_TOWER_OF_LIFE_DESTROYED      = 21030
+};
+
+enum eAchievementCriteria
+{
+    ACHIEVEMENT_UNBROKEN_10                              = 10044, // Leviathan
+    ACHIEVEMENT_UNBROKEN_25                              = 10045,
+    ACHIEVEMENT_CRITERIA_SHUTOUT_10                      = 10054,
+    ACHIEVEMENT_CRITERIA_SHUTOUT_25                      = 10055,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_CHOPPER_10         = 10046,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_SIEGE_10           = 10047,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_DEMOLISHER_10      = 10048,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_CHOPPER_25         = 10049,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_SIEGE_25           = 10050,
+    ACHIEVEMENT_CRITERIA_3_CAR_GARAGE_DEMOLISHER_25      = 10051,
+    ACHIEVEMENT_CRITERIA_HOT_POCKET_10                   = 10430, // Ignis
+    ACHIEVEMENT_CRITERIA_HOT_POCKET_25                   = 10431,
+    ACHIEVEMENT_CRITERIA_QUICK_SHAVE_10                  = 10062, // Razorscale
+    ACHIEVEMENT_CRITERIA_QUICK_SHAVE_25                  = 10063,
+};
+
+#endif
